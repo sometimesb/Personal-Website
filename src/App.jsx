7f4a94b563d85b9275dc14382c6c54c7f3aa0d30
sometimesb <77695101+sometimesb@users.noreply.github.com>
2024-03-03 12:53:17 -0600

    finished nav bar on PC, need to create for mobile view.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+export default function Navbar(){
+  return(
+    <nav>
+      <h3 className="logo">Bilal.dev</h3>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
