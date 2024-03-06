9ef7d12131bb36f478a690bb469e13f7408e2d80
sometimesb <77695101+sometimesb@users.noreply.github.com>
2024-03-06 10:03:35 -0600

    finished about me section with responsiveness.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,6 +1,6 @@
 export default function AboutMe(){
   return(
-<div className="about-container">
+<div id="About" className="about-container">
   <div className="about-content">
     <div className="img-container">
       <div className="about-img"></div>
@@ -8,9 +8,10 @@ export default function AboutMe(){
     <div className="about-me-container">
       <div className="about-me">
         <h1 className="h1-about-me">ABOUT ME</h1>
-        <h1 className="h1-dev-type">Full-Stack Engineer based in Nashville, Tennessee 📍</h1>
+        <h1 className="h1-dev-type">Full-Stack Engineer based in Nashville, Tennessee</h1>
         <p className="p-dev-info">I am Bilal (B)! I am a Mechatronics Engineer that took a CS course out of curiosity, and now we here are! Tons of credits later 🎉</p>
-        <p className="p-dev-info">I graduated from Middle Tennessee State University at the Age of 20 and Rank 1!</p>
+        <p className="p-dev-info">I graduated from Middle Tennessee State University at the Age of 20 and Rank 1.</p>
+        <p className="p-dev-info">I currently use SQL and VB.NET at my current position and use HTML, CSS, JS, React, and PHP in projects.</p>
       </div>
     </div>
   </div>
